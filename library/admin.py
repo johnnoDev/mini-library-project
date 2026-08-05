e4f901b9c245e7f03ba55e4301a4a6a320f90231
johnnoDev <johnny.lopezvera20@gmail.com>
2026-08-04 22:17:55 -0500

    inline agregado

--- library/admin.py
+++ library/admin.py
@@ -3,8 +3,18 @@ from .models import Author, Book, Genre, BookDetail, Review, Loan
 
 # Register your models here.
 
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 1
+
+class BookDetailInline(admin.StackedInline):
+    model = BookDetail
+    can_delete = False
+    verbose_name_plural = 'Detalle del libro'
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
+    inlines = [ReviewInline, BookDetailInline]
     list_display = ('title', 'author', 'publication_date', 'isbn') # Columnas que se mostrarán en admin/.../book
     search_fields = ('title', 'author__name') # Crea un campo de búsqueda con los campos asignados
     list_filter = ('author', 'genres', 'publication_date') # Crea un panel lateral derecho con filtros rápidos
